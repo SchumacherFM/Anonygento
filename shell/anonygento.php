c27083fadb4361a80e8f04c28a92ba99ae600cf3
Cyrill Schumacher <Cyrill@Schumacher.fm>
2012-12-22 19:19:40 +0100

    Updated license

--- shell/anonygento.php
+++ shell/anonygento.php
@@ -1,48 +1,14 @@
 <?php
 /**
- * Magento
- *
- * NOTICE OF LICENSE
- *
- * This source file is subject to the Open Software License (OSL 3.0)
- * that is bundled with this package in the file LICENSE.txt.
- * It is also available through the world-wide-web at this URL:
- * http://opensource.org/licenses/osl-3.0.php
- * If you did not receive a copy of the license and are unable to
- * obtain it through the world-wide-web, please send an email
- * to license@magentocommerce.com so we can send you a copy immediately.
- *
- * DISCLAIMER
- *
- * Do not edit or add to this file if you wish to upgrade Magento to newer
- * versions in the future. If you wish to customize Magento for your
- * needs please refer to http://www.magentocommerce.com for more information.
- *
- * @category    Mage
- * @package     Mage_Shell
- * @copyright   Copyright (c) 2009 Irubin Consulting Inc. DBA Varien (http://www.varien.com)
+ * @category    SchumacherFM_Anonygento
+ * @package     Model
+ * @author      Cyrill at Schumacher dot fm
  * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * @bugs        https://github.com/SchumacherFM/Anonygento/issues
  */
 
 require_once 'abstract.php';
-error_reporting(E_ALL);
 
-//use SchumacherFM\Anonygento\Zend\Console\Prompt;
-//
-//$confirm = new Prompt\Confirm('Are you sure you want to continue?');
-//$result = $confirm->show();
-//if ($result) {
-//    // the user chose to continue
-//}
-//exit;
-
-/**
- * Magento Anonygento Script
- *
- * @category    SchumacherFM_Anonygento_Anonygento
- * @package     shell
- * @author      Cyrill at Schumacher dot fm
- */
 class Mage_Shell_Anonygento extends Mage_Shell_Abstract
 {
 
