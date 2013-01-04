6309e6f5f0a12014e7b8f0bfaad8411203ff0dfa
Cyrill Schumacher <Cyrill@Schumacher.fm>
2013-01-04 07:37:26 +0100

    Update demo1 model

--- demo1/app/code/local/SchumacherFM/Demo1/Model/Mydemo1.php
+++ demo1/app/code/local/SchumacherFM/Demo1/Model/Mydemo1.php
@@ -2,12 +2,8 @@
 
 class SchumacherFM_Demo1_Model_Mydemo1 extends Varien_Object
 {
-
     public function changeMydemo1($mydemo1)
     {
-
         return str_replace('Init', 'Anon', $mydemo1);
-
     }
-
 }
