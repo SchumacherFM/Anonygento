399b41a1e62b1f2bd056d4ee9482898ff4c12555
Cyrill Schumacher <Cyrill@Schumacher.fm>
2013-01-08 18:26:57 +0100

    Update demo2

--- demo/app/code/community/SchumacherFM/Demo2/Model/Observer.php
+++ demo/app/code/community/SchumacherFM/Demo2/Model/Observer.php
@@ -12,6 +12,22 @@ class SchumacherFM_Demo2_Model_Observer
         $event      = $observer->getEvent();
         $collection = $event->getCollection();
 
+        $option = array(
+            'value' => 'catalogProduct',
+            'model' => 'catalog/product'
+        );
+
+        $rowCountModel = Mage::getModel('schumacherfm_anonygento/counter');
+
+        $optObj = new Varien_Object($option);
+        $optObj
+        /* @see SchumacherFM_Anonygento_Block_Adminhtml_Anonygento_Grid column: Status */
+            ->setStatus(Mage::helper('schumacherfm_anonygento')->getAnonymizations($option['value']))
+            ->setUnanonymized($rowCountModel->unAnonymized($option['model']))
+            ->setAnonymized($rowCountModel->anonymized($option['model']));
+
+        $collection->addItem($optObj);
+
         Zend_Debug::dump($collection);
         exit;
 
