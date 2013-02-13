1bb597d81f3cd0efbd905ae8de11a4d22c1cf49d
Cyrill Schumacher <Cyrill@Schumacher.fm>
2013-02-13 07:34:16 +0100

    Adding Unit Tests, base set up

---
+++ src/app/code/community/SchumacherFM/Anonygento/Test/Model/Counter.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @category    SchumacherFM_Anonygento
+ * @package     Test
+ * @author      Cyrill at Schumacher dot fm
+ * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * @bugs        https://github.com/SchumacherFM/Anonygento/issues
+ */
+
+class SchumacherFM_Anonygento_Test_Model_Counter extends EcomDev_PHPUnit_Test_Case
+{
+
+
+}
