ef1d3d37ea818ba4f7712700f5cd3603329c10a2
Cyrill Schumacher <Cyrill@Schumacher.fm>
2013-02-14 08:28:22 +0100

    test for anonymization/customer

---
+++ src/app/code/community/SchumacherFM/Anonygento/Test/Model/Anonymizations/Customer.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @category    SchumacherFM_Anonygento
+ * @package     Test
+ * @author      Cyrill at Schumacher dot fm
+ * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * @bugs        https://github.com/SchumacherFM/Anonygento/issues
+ */
+class SchumacherFM_Anonygento_Test_Model_Anonymizations_Customer extends EcomDev_PHPUnit_Test_Case
+{
+
+    /**
+     * @test
+     * @loadFixture
+     */
+    public function testRun()
+    {
+        $this->assertTrue(TRUE);
+    }
+}
